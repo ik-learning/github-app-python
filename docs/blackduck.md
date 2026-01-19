ba8f0564a156005c2fdaadc91aba0a684840e67d
ivan katliarchuk <ivan.katliarchuk@gmail.com>
2026-01-19 22:37:56 +0000

    make all docker images ready
    Signed-off-by: ivan katliarchuk <ivan.katliarchuk@gmail.com>

---
+++ docs/blackduck.md
@@ -0,0 +1,5 @@
+# BlackDuck docs
+
+- https://repo.blackduck.com/bds-integrations-release/com/blackduck/integration/bridge/binaries/bridge-cli-thin-client/latest/
+- https://documentation.blackduck.com/bundle/bridge/page/documentation/c_using-bridge-with-black-duck.html
+- https://github.com/blackducksoftware/hub-rest-api-python/blob/master/examples/get_licenses.py
--- infra/helpers.mk
+++ infra/helpers.mk
@@ -1,7 +1,11 @@
 # Determine the directory of this helper makefile
-INFRASTRUCTURE_DIR := infrastructure
+INFRA_DIR := infra
 
 docker-python-deps: ## Build dockerfile.python to install python dependencies
-    docker build -f $(INFRASTRUCTURE_DIR)/Dockerfile.python -t github-app
-	docker-compose -f $(INFRASTRUCTURE_DIR)/docker-compose.yml up --build
+	@docker build -f $(INFRA_DIR)/Dockerfile.python -t python-deps .
 
+docker-smee: ## Build dockerfile.smee to create smee client image
+	@docker build -f $(INFRA_DIR)/Dockerfile.smee -t nodejs-smee .
+
+docker-blackduck: ## Build dockerfile.blackduck to create blackduck client image
+	@docker build -f $(INFRA_DIR)/Dockerfile.blackduck -t blackduck-deps .
